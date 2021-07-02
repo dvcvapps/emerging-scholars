4ba9a5b13d3119472231615cde342926f0a83416
David <dvcv1992@gmail.com>
2021-07-01 23:59:51 -0700

    Remove react bootstrap

--- src/components/MainNavbar.jsx
+++ src/components/MainNavbar.jsx
@@ -1,26 +1,14 @@
 import React from 'react';
-import { Navbar, Nav } from 'react-bootstrap';
 import { Link } from "react-router-dom";
 
 function MainNavbar(props) {
   return (
-    <Navbar collapseOnSelect expand="md" bg="dark" variant="dark">
-      <Navbar.Brand>
-        <Link to="/">Home</Link>
-      </Navbar.Brand>
-      <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-      <Navbar.Collapse id="responsive-navbar-nav">
-        <Nav className="mr-auto">
-          <Nav><Link to="/about">About</Link></Nav>
-          <Link to="/board_members">Board Members</Link>
-        </Nav>
-        <Nav>
-          <Nav.Link >
-          <Link to="/donate">Donate</Link>
-          </Nav.Link>
-        </Nav>
-      </Navbar.Collapse>
-    </Navbar>
+    <div>
+      <Link to="/">Home</Link>
+      <Link to="/about">About</Link>
+      <Link to="/board_members">Board Members</Link>
+      <Link to="/donate">Donate</Link>
+    </div>
   )
 }
 
