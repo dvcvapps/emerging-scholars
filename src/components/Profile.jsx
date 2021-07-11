a945ba7965bb58d271e5fcf1c2c3eae16101917d
David <dvcv1992@gmail.com>
2021-07-11 00:25:57 -0700

    Status

---
+++ src/components/Profile.jsx
@@ -0,0 +1,35 @@
+/* eslint-disable no-unused-vars */
+/* eslint-disable consistent-return */
+import React from 'react';
+import PropTypes from 'prop-types';
+
+function Profile({ header, image, desc }) {
+  return (
+    <div className="d-flex justify-content-around flex-column">
+      <h2 className="text-center my-5 h2">{header}</h2>
+      <img
+        className="text-center rounded-circle z-depth-2"
+        alt="100x100"
+        src={image}
+        data-holder-rendered="true"
+      />
+      <p className="text-center my-5">{desc}</p>
+    </div>
+  );
+}
+
+Profile.propTypes = {
+  header: PropTypes.string,
+  image: PropTypes.string,
+  desc: PropTypes.string,
+
+};
+
+Profile.defaultProps = {
+  header: '',
+  image: '',
+  desc: '',
+
+};
+
+export default Profile;
--- src/pages/BoardMembers.jsx
+++ src/pages/BoardMembers.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import Spacer from '../components/Spacer';
+import Profile from '../components/Profile';
 
 function BoardMembers() {
   return (
@@ -7,6 +8,43 @@ function BoardMembers() {
       <Spacer />
       <div className="container" style={{ minHeight: 1000 }}>
         <h1 className="text-center">Board Members</h1>
+        <div className="row">
+          <div className="col-4">
+            <Profile
+              header="Fake Name"
+              image="https://mdbootstrap.com/img/Photos/Avatars/img%20(31).jpg"
+              desc="Description of person."
+            />
+          </div>
+          <div className="col-4">
+            <Profile
+              header="Fake Name"
+              image="https://mdbootstrap.com/img/Photos/Avatars/img%20(31).jpg"
+              desc="Description of person."
+            />
+          </div>
+          <div className="col-4">
+            <Profile
+              header="Fake Name"
+              image="https://mdbootstrap.com/img/Photos/Avatars/img%20(31).jpg"
+              desc="Description of person."
+            />
+          </div>
+          <div className="col-4">
+            <Profile
+              header="Fake Name"
+              image="https://mdbootstrap.com/img/Photos/Avatars/img%20(31).jpg"
+              desc="Description of person."
+            />
+          </div>
+          <div className="col-4">
+            <Profile
+              header="Fake Name"
+              image="https://mdbootstrap.com/img/Photos/Avatars/img%20(31).jpg"
+              desc="Description of person."
+            />
+          </div>
+        </div>
       </div>
     </>
   );
