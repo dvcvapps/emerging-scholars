7dffce6e8739ff534103eb78532647901a099289
David <dvcv1992@gmail.com>
2021-07-13 01:08:26 -0700

    Add donate test button

--- src/pages/Donate.jsx
+++ src/pages/Donate.jsx
@@ -7,6 +7,11 @@ function Donate() {
       <Spacer />
       <div className="container" style={{ minHeight: 1000 }}>
         <h1 className="text-center text-secondary">Donate</h1>
+        <p className="text-center text-danger">TEST DO NOT DONATE</p>
+        <div className="d-flex justify-content-center">
+          <div className="gfm-embed" style={{ maxWidth: 'max-content' }} data-url="https://www.gofundme.com/f/emerging-scholars/widget/medium/" />
+        </div>
+        <p className="text-center text-danger">TEST DO NOT DONATE</p>
       </div>
     </>
   );
