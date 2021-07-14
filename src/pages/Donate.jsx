0b8d5659038a4b46f40718d5a38ad239381c2e2a
David <dvcv1992@gmail.com>
2021-07-14 01:16:11 -0700

    Remove donate button

--- src/pages/Donate.jsx
+++ src/pages/Donate.jsx
@@ -7,11 +7,10 @@ function Donate() {
       <Spacer />
       <div className="container" style={{ minHeight: 1000 }}>
         <h1 className="text-center text-secondary">Donate</h1>
-        <p className="text-center text-danger">TEST DO NOT DONATE</p>
+        <p className="text-center text-danger">DOES NOT WORK YET</p>
         <div className="d-flex justify-content-center">
-          <div className="gfm-embed" style={{ maxWidth: 'max-content' }} data-url="https://www.gofundme.com/f/emerging-scholars/widget/medium/" />
+          <button type="button" className="btn btn-warning">DONATE</button>
         </div>
-        <p className="text-center text-danger">TEST DO NOT DONATE</p>
       </div>
     </>
   );
